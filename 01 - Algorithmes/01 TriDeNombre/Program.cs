c34db2daea0a12da0db5272dbaffad33815e876c
KARAN PARIYAR <krn1052@gmail.com>
2024-09-23 11:55:35 +0200

    Fin TriDeNombre

--- 01 - Algorithmes/01 TriDeNombre/Program.cs
+++ 01 - Algorithmes/01 TriDeNombre/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace TriDeNombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lire les trois nombres entiers
+            Console.WriteLine("Entrez le premier nombre (A) :");
+            int A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entrez le deuxième nombre (B) :");
+            int B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entrez le troisième nombre (C) :");
+            int C = int.Parse(Console.ReadLine());
+
+            // Mettre les nombres dans un tableau
+            int[] nombres = { A, B, C };
+
+            // Trier le tableau
+            Array.Sort(nombres);
+
+            // Afficher les nombres dans l'ordre croissant
+            Console.WriteLine("Les nombres dans l'ordre croissant sont :");
+            foreach (int nombre in nombres)
+            {
+                Console.WriteLine(nombre);
+            }
+        }
+    }
+}
+
