edc47b334a68c9fc4d04612ab9d45313344cc3f6
KARAN PARIYAR <krn1052@gmail.com>
2024-09-24 16:21:42 +0200

    updated

---
+++ 01 - Algorithmes/Algorithmes/EsTuMajeur/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Entrez votre âge: ");
+        int age = int.Parse(Console.ReadLine());
+
+        if (age < 0)
+        {
+            Console.WriteLine("Vous n’êtes pas encore né");
+        }
+        else if (age < 18)
+        {
+            Console.WriteLine("Vous êtes mineur");
+        }
+        else
+        {
+            Console.WriteLine("Vous êtes majeur");
+        }
+    }
+}
+
