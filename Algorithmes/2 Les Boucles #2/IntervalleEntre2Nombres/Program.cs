0ba49fccd2ae3612cd588e83b3d898af74b2dae8
KARAN PARIYAR <krn1052@gmail.com>
2024-09-25 15:01:02 +0200

    exercise IntervalleEntre2Nombres

---
+++ Algorithmes/2 Les Boucles #2/IntervalleEntre2Nombres/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class IntervalleEntre2Nombres
+{
+    static void Main()
+    {
+        Console.WriteLine("Veuillez saisir le nombre A : ");
+        int A = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Veuillez saisir le nombre B : ");
+        int B = int.Parse(Console.ReadLine());
+
+        if (A < B)
+        {
+
+            for (int i = A + 1; i < B; i++)
+            {
+                Console.WriteLine(i + " ");
+            }
+        }
+
+        else if (A > B)
+        {
+
+            for (int i = A - 1; i > B; i--)
+            {
+                Console.WriteLine(i + " ");
+            }
+
+        }
+
+        else
+        {
+            Console.WriteLine("A et B sont égaux, il n'y a pas de nombres entre eux.");
+        }
+    }
+}
