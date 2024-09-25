c684e4c8b5aed3879586e17c3810339b0a9f21b6
KARAN PARIYAR <krn1052@gmail.com>
2024-09-25 09:38:46 +0200

    update

--- Algorithmes/MaRetraite/Program.cs
+++ Algorithmes/MaRetraite/Program.cs
@@ -1,6 +1,31 @@
-﻿
+﻿using System;
 
-int data = 550;
-string name = "Karan";
+class MaRetraite
+{
+    static void Main()
+    {
+        Console.Write("Entrez votre âge: ");
+        int age;
+        bool isValid = int.TryParse(Console.ReadLine(), out age);
 
-Console.WriteLine( data + "and" + name);
+        if (!isValid || age < 0)
+        {
+            Console.WriteLine("La valeur fournie n’est pas un âge valide.");
+        }
+        else if (age < 60)
+        {
+            int anneesRestantes = 60 - age;
+            Console.WriteLine($"Il vous reste {anneesRestantes} années avant la retraite.");
+        }
+        else if (age == 60)
+        {
+            Console.WriteLine("C’est le moment de prendre sa retraite.");
+        }
+        else
+        {
+            int anneesDeRetraite = age - 60;
+            Console.WriteLine($"Vous êtes à la retraite depuis {anneesDeRetraite} années.");
+        }
+    }
+}
+
--- Algorithmes/TriDeNombresPlus/Program.cs
+++ Algorithmes/TriDeNombresPlus/Program.cs
@@ -1,15 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-using System;
-
-namespace TriDeNombre
+namespace TriDeNombrePlus
 {
-    class Program
+
+    class App
     {
+
         static void Main(string[] args)
         {
-            // Lire les trois nombres entiers
-            Console.WriteLine("Entrez le premier nombre (A) :");
+            Console.WriteLine("Entrez le premier nombre (A) : ");
             int A = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Entrez le deuxième nombre (B) :");
@@ -18,19 +17,19 @@ namespace TriDeNombre
             Console.WriteLine("Entrez le troisième nombre (C) :");
             int C = int.Parse(Console.ReadLine());
 
-            // Mettre les nombres dans un tableau
             int[] nombres = { A, B, C };
 
-            // Trier le tableau
             Array.Sort(nombres);
+            Array.Reverse(nombres);
 
-            // Afficher les nombres dans l'ordre croissant
-            Console.WriteLine("Les nombres dans l'ordre croissant sont :");
-            foreach (int nombre in nombres)
+            Console.WriteLine("Les nombres dans l'ordre decroissant sont :");
+            foreach(int nombre in nombres)
             {
                 Console.WriteLine(nombre);
             }
-        }
-    }
-}
 
+        }
+
+    }
+
+}
