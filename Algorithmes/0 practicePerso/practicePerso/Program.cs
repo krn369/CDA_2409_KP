68dd1f0c09f3b9da5b25d24b2ba335c484875250
KARAN PARIYAR <krn1052@gmail.com>
2024-09-27 09:11:51 +0200

    exercice RechercheDesDiviseurs

---
+++ Algorithmes/0 practicePerso/practicePerso/Program.cs
@@ -0,0 +1,3 @@
+﻿using System;
+Console.WriteLine("Hello karan is it good for you to read this text!!");
+Console.WriteLine( "You are the BOSS!!!");
---
+++ Algorithmes/RechercheDesDiviseurs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class RechercheDesDiviseurs
+{
+    static void Main(string[] args)
+    {
+        int nombre;
+        int diviseur;
+
+
+        Console.WriteLine("Saisissez un nombre entier : ");
+        nombre =  int.Parse(Console.ReadLine());
+
+        if (nombre <= 1)
+        {
+            Console.WriteLine("Le nombre n'a pas de diviseurs autres que 1 et lui-memê.");
+        }
+
+        else
+        {
+            Console.WriteLine("Divisores : ");
+            for (diviseur = 2; diviseur <= nombre; diviseur++)
+            {
+
+                if (nombre % diviseur == 0)
+                {
+                    Console.WriteLine(diviseur + " ");
+                }
+            }
+        }
+
+    }
+
+}
+
