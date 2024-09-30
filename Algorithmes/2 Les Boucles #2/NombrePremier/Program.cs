7503a855a4615f7fcbb1a51c11a73d19ffac77f1
KARAN PARIYAR <krn1052@gmail.com>
2024-09-30 09:01:34 +0200

    update

---
+++ Algorithmes/2 Les Boucles #2/NombrePremier/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class NombrePremier
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Saisissez un nombre positif supérieur à 1: ");
+        int N = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Les nombres premiers compris entre 1 et {N} sont:");
+        for (int i = 2; i <= N; i++)
+        {
+            if (estPremier(i))
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        // Check if the entered number itself is prime
+        if (estPremier(N))
+        {
+            Console.WriteLine($"\n{N} est un nombre premier.");
+        }
+        else
+        {
+            Console.WriteLine($"\n{N} n'est pas un nombre premier.");
+        }
+    }
+
+    static bool estPremier(int n)
+    {
+        if (n <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
+
