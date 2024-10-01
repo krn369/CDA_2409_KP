a968ea581fbf778dfe5ee838ba1fea71125c109d
KARAN PARIYAR <krn1052@gmail.com>
2024-10-01 16:59:59 +0200

    Exercice 6.3 : Calcul du nombre de personnes

---
+++ Algorithmes/4 Révisions #2/CalculDuNombreDP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class CalculDuNombreDJ
+    {
+    static void Main(string[] args)
+    {
+        int countJeune = 0;
+        int[] ages = new int[20];
+
+        Console.WriteLine("Veuillez saisir les âges de 20 personnes : ");
+
+        for (int i = 0; i < ages.Length; i++)
+        {
+            Console.Write($"Age de la personne {i + 1} : ");
+            ages[i] = int.Parse(Console.ReadLine());
+
+            if (ages[i] < 20)
+            {
+                countJeune++;            
+            }
+        }
+
+        Console.WriteLine($"Nombre de jeunes (moins de 20 ans) : {countJeune}");
+    }
+}
