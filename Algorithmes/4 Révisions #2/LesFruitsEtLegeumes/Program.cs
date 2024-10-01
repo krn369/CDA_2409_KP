2f9958be17fb369dbe9537a9e17ea10427c3587a
KARAN PARIYAR <krn1052@gmail.com>
2024-10-01 10:25:41 +0200

    Exercice 6.1 : Les fruits et légumes

---
+++ Algorithmes/4 Révisions #2/LesFruitsEtLegeumes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class LesFruitsEtLegeumes
+{
+    static void Main(string[] args)
+    {
+        List<string> fruitsLegumes = new List<string>();
+        List<double> prixKilo = new List<double>();
+
+        while (true)
+        {
+            Console.WriteLine("Saisissez le nom d'un fruit ou légume (ou \"go\" pour terminer) : ");
+            string nomFruitsLegumes = Console.ReadLine();
+
+            if (nomFruitsLegumes.Equals("go", StringComparison.OrdinalIgnoreCase))
+            {
+                break;
+            }
+
+
+            Console.WriteLine("Saisissez le prix au kilo : ");
+            double prix = double.Parse(Console.ReadLine());
+
+            fruitsLegumes.Add(nomFruitsLegumes);
+            prixKilo.Add(prix);
+
+        }
+
+        Console.WriteLine("\nListe des fruits et légumes :\n");
+        for (int i = 0; i < fruitsLegumes.Count; i++)
+        {
+            Console.WriteLine($"1 kilogramme de {fruitsLegumes[i]} coûte {prixKilo[i]:F2} euros.");
+        }
+
+        int indiceFruitsLegumesMoinsCher = 0;
+        for (int i = 1; i < prixKilo.Count; i++)
+        {
+            if (prixKilo[i] < prixKilo[indiceFruitsLegumesMoinsCher])
+            {
+                indiceFruitsLegumesMoinsCher = i;
+            }
+        }
+
+        Console.WriteLine("\nLégume le moins cher au kilo : " + fruitsLegumes[indiceFruitsLegumesMoinsCher]);
+
+    }
+}
