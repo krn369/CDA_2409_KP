a9a2ee770c89f29d0d916e74e0d15c354649c81f
KARAN PARIYAR <krn1052@gmail.com>
2024-10-10 11:17:39 +0200

    update

---
+++ Algorithmes/5 Extra/Collection.Listes/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enregistrement de nouveaux utilisateurs");
+
+        string[] utilisateurs;
+
+        string saisieNomPrenom;
+
+        char saisieOuiNon;
+
+        string[] tabTempo;
+
+        utilisateurs = new string[1] { "Karan Pariyar" };
+
+        do
+        {
+            Console.WriteLine("Saisissez votre prénom et nom :");
+
+            saisieNomPrenom = Console.ReadLine()?? " ";
+
+            tabTempo = utilisateurs;
+
+            utilisateurs = new string[utilisateurs.Length + 1];
+
+            tabTempo.CopyTo(utilisateurs, 0);
+
+            utilisateurs[utilisateurs.Length - 1] = saisieNomPrenom;
+
+            Console.WriteLine("Souhaitez vous ajouter un autre utilisateur ? (N/0)");
+
+            saisieOuiNon = Console.ReadKey().KeyChar;
+
+        }
+        while (saisieOuiNon == 'o'  || saisieOuiNon == 'O');
+    }
+}
