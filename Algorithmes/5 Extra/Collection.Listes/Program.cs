597b37d5f87534bd72235c5eadc79e706e70b51c
KARAN PARIYAR <krn1052@gmail.com>
2024-10-10 12:21:15 +0200

    arraylist

--- Algorithmes/5 Extra/Collection.Listes/Program.cs
+++ Algorithmes/5 Extra/Collection.Listes/Program.cs
@@ -19,41 +19,92 @@ Au démarrage, il n'y a aucun utilisateur enregistré.
 
 */
 
+
+/*
+    using System.Collections;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Enregistrement de nouveaux utilisateurs");
 
-        string[] utilisateurs;
+        List<string> utilisateurs = new List<string>();  // Liste fortement typée
 
         string saisieNomPrenom;
 
         char saisieOuiNon;
 
-        string[] tabTempo;
-
-        utilisateurs = new string[1] { "Karan Pariyar" };
-
         do
         {
             Console.WriteLine("Saisissez votre prénom et nom :");
 
-            saisieNomPrenom = Console.ReadLine()?? " ";
+            saisieNomPrenom = Console.ReadLine() ?? " ";
 
-            tabTempo = utilisateurs;
+            utilisateurs.Add(saisieNomPrenom);
 
-            utilisateurs = new string[utilisateurs.Length + 1];
+            Console.WriteLine("Souhaitez vous ajouter un autre utilisateur ? (N/O)");
 
-            tabTempo.CopyTo(utilisateurs, 0);
-
-            utilisateurs[utilisateurs.Length - 1] = saisieNomPrenom;
-
-            Console.WriteLine("Souhaitez vous ajouter un autre utilisateur ? (N/0)");
-
-            saisieOuiNon = Console.ReadKey().KeyChar;
+            saisieOuiNon = Console.ReadKey(true).KeyChar;
 
         }
-        while (saisieOuiNon == 'o'  || saisieOuiNon == 'O');
+        while (saisieOuiNon == 'o' || saisieOuiNon == 'O');
+
+        for (int i = 0; i < utilisateurs.Count; i++)
+        {
+            Console.WriteLine(utilisateurs[i]);
+        }
+
+        /*
+        foreach (string personne in utilisateurs)
+        {
+            Console.WriteLine(personne);
+        }
+        *
+
     }
+}
+
+*/
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<User> users = new List<User>();  // to keep track of all the users we will add.
+        bool addMoreUsers = true;
+
+        while (addMoreUsers) // It continues to run as long as addMoreUsers is true.
+        {
+            // Demander le nom et le prénom de l'utilisateur // Ask the user to enter their fname and lname.
+            Console.Write("Entrez le nom: ");
+            string lastName = Console.ReadLine().ToUpper();
+            Console.Write("Entrez le prénom: ");
+            string firstName = Console.ReadLine().ToUpper();
+
+            // Enregistrer l'utilisateur  || Create an new user object with the entered fname and lname and add it to the list.
+            users.Add(new User { FirstName = firstName, LastName = lastName });
+
+            // Demander si l'utilisateur souhaite ajouter une autre personne  || check if more users should be added, if type "oui" the loop continues, otherwise it stops
+            Console.Write("Voulez-vous ajouter une autre personne? (oui/non): ");
+            string response = Console.ReadLine().ToLower();
+            addMoreUsers = response == "oui";
+        }
+
+        // Afficher tous les utilisateurs enregistrés || Displaying all users after the loop ends
+        Console.WriteLine("\nUtilisateurs enregistrés:");
+        foreach (var user in users)
+        {
+            Console.WriteLine($"{user.FirstName} {user.LastName}");
+        }
+    }
+}
+
+class User  // user class defination; it has two properties, fname and lname to store the user's information.
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 }
