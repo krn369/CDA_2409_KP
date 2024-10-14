744f4b522d9965cfad865e1045234278f1a961c4
KARAN PARIYAR <krn1052@gmail.com>
2024-10-14 11:54:50 +0200

    update

--- Algorithmes/0 practicePerso/CalculateFinalGPA/Program.cs
+++ Algorithmes/0 practicePerso/CalculateFinalGPA/Program.cs
@@ -1,6 +1,7 @@
 ﻿/*
 Calculate the final GPA and modify the console output to achieve the desired reporting format. 
 The GPA is equal to the sum of the total grade points divided by the sum of the total credit hours.
+Your goal was to build an application that takes course information for a student, calculates the overall GPA, and displays the results.
  */
 
 string studentName = "Sophia Johnson";
