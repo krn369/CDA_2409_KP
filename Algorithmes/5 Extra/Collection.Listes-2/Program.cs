d4cac1a253f1803d3efafdfaffd0fde5133d6c5e
KARAN PARIYAR <krn1052@gmail.com>
2024-10-15 12:15:12 +0200

    update

--- Algorithmes/5 Extra/Collection.Listes-2/Program.cs
+++ Algorithmes/5 Extra/Collection.Listes-2/Program.cs
@@ -43,24 +43,31 @@ using System.Security.Cryptography.X509Certificates;
 // The USER class is a blueprint for creating user objects. Each user has a fname, lname, birthdate and additional information (profession or favourite color)
 class User
 {
-    // { get; set; } This is shorthand for creating a property with a getter and setter. A getter allows to read the value of the property
-    // a setter allows to assign a value to the property
+    // { get; set; } This is shorthand for creating a property with a getter and setter.
+    // A getter allows to read the value of the property
+    // A setter allows to assign a value to the property
     public string Prenom { get; set; }
     public string Nom { get; set; }
     public DateTime DateDeNaissance { get; set; }
     public string InfoAdditional { get; set; }
+
+    // This property calculates the age based on the birthdate.
     public int Age => Calculer_Age(DateDeNaissance);
 
 
-    // This method is used within the USER class to ensure the age is calculated correctly.
+    // This method calculates the age based on the birthdate.
     private int Calculer_Age(DateTime dateDeNaissance)
     {
+        // Calculate the initial age bye subtracting the birth year form the current year.
         int age = DateTime.Now.Year - dateDeNaissance.Year;
 
- // Checks If the current day of the year is before the birthday, it means the person hasn’t had their birthday yet this year, so subtract 1 from the age.
-       
+        // Checks If the current day of the year is before the birthday.
+        // If it is, subtract 1 from the age because the person hasn't had their birthday yet this year.
+
         if (DateTime.Now.DayOfYear < dateDeNaissance.DayOfYear)
             age--;
+
+        // Return the calculated age.
         return age;
     }
 
@@ -71,14 +78,17 @@ class Program
 {
     static void Main()
     {
-        List<User> users = new List<User>();  // To keep track of all the users which will be added.
-        bool addMoreUsers = true;
+        //Create a list to keep track of all the users that will be added.
+        List<User> users = new List<User>();
+        bool addMoreUsers = true; // This variable controls whether more users should be added.
 
-        while (addMoreUsers) // It continues to run as long as addMoreUsers is true.
+        // Continue to run the loop as long as addMoreUsers is true.
+        while (addMoreUsers)
         {
-            // Demander le nom et le prénom de l'utilisateur || Ask the user to enter their fname and lname.
+            // Ask the user to enter their lname and fname.
             Console.Write("Entrez le nom: ");
             string nom = Console.ReadLine().ToUpper();
+
             Console.Write("Entrez le prénom: ");
             string prenom = Console.ReadLine().ToUpper();
 
@@ -86,38 +96,45 @@ class Program
             Console.Write("Entrez la date de naissance (yyyy-mm-dd) : ");
             DateTime dateDeNaissance = DateTime.Parse(Console.ReadLine());
 
-            // Enregistrer l'utilisateur  || Create an new user object with the entered information and add it to the list.
-            string infoAdditional;
+            // Create a new user object with the entered information.            
             User user = new User { Prenom = prenom, Nom = nom, DateDeNaissance = dateDeNaissance };
 
+            // Declare a variable to store additional information.
+            string infoAdditional;
+
+            // If the user is an adult, ask for their profession.
             if (user.Age >= 18)
             {
-                // Ask for the user's profession if they are an adult.
                 Console.Write("Entrez votre métier : ");
                 infoAdditional = Console.ReadLine().ToUpper();
             }
+            // Ask fo the user's favourite color if ther are a minor.
             else
             {
-                // Ask fo the user's favourite color if ther are a minor.
                 Console.Write("Entrez votre couleur préférée : ");
                 infoAdditional = Console.ReadLine().ToUpper();
             }
 
+            // Assign the additional information to the user object.
             user.InfoAdditional = infoAdditional;
+
+            // Add the user object to the list of users.
             users.Add(user);
 
-
-            // Demander si l'utilisateur souhaite ajouter une autre personne  || check if more users should be added, if type "oui" the loop continues, otherwise it stops
+            // Ask if the user wants to add another person.
             Console.Write("\nVoulez-vous ajouter une autre personne? (oui/non): ");
             string response = Console.ReadLine().ToLower();
+
+            // If the reponse is "oui", continue the loop. Otherwise, stop the loop.
             addMoreUsers = response == "oui";
             Console.WriteLine();
         }
 
-        // Afficher tous les utilisateurs enregistrés || Displaying all registered users after the loop ends
+        // Display all registered users after the loop ends
         Console.WriteLine("\nUtilisateurs enregistrés: ");
         foreach (var user in users)
         {
+            // Determine the type of additional information based on the user's age.
             String infoType = user.Age >= 18 ? "Métier" : "Couleur préférée";
             Console.WriteLine($"\n{user.Prenom} {user.Nom} - {user.DateDeNaissance.ToShortDateString()} ({user.Age} ans) - {infoType} :  {user.InfoAdditional}");
         }
