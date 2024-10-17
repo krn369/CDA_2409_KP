02e614801b1c4e2d448b10cd7bfc2a948be3903b
KARAN PARIYAR <krn1052@gmail.com>
2024-10-17 09:50:20 +0200

    update

---
+++ Algorithmes/5 Extra/Collection.Listes.Object/Program.cs
@@ -0,0 +1,91 @@
+﻿using Collection.Listes.Object;
+using System;
+
+internal class MyClass
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enregistrement d'utilisateurs (gestion avec Objet Métier).");
+
+        //Variable
+        string saisieNomPrenom;
+        string saisieDate;
+        string? metierCouleur = null;
+        ConsoleKey continuerO_N;
+        List<Utilisateur> utilisateurs = new();
+
+        //Traitement
+        do
+        {
+
+            try
+            {
+                Console.WriteLine("\nSaisissez le nom et Prénom.");
+                saisieNomPrenom = Console.ReadLine().ToUpper();
+
+                Console.WriteLine("\nSaisissez la date de naissane, jour/mois/année.");
+                saisieDate = Console.ReadLine();
+
+                Utilisateur unUtilisateur = new Utilisateur(saisieNomPrenom, saisieDate);
+
+                if (unUtilisateur.IsMajeur())
+                {
+                    Console.WriteLine("\nVotre metier ?");
+                    metierCouleur = Console.ReadLine();
+                    unUtilisateur.SetMetier(metierCouleur);
+                }
+                else
+                {
+                    Console.WriteLine("\ncouleur préféré ?");
+                    metierCouleur = Console.ReadLine();
+                    unUtilisateur.SetCouleurPreferee(metierCouleur);
+                }
+
+
+                utilisateurs.Add(unUtilisateur);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("\nVoulez vous saisir un autre utilisateur : N/O ?");
+
+            continuerO_N = Console.ReadKey(true).Key;
+        }
+        while (continuerO_N == ConsoleKey.O);
+
+
+        // Affichage de la liste des utilisateurs
+
+        // List<Utilisateur]> utilisateurs = new();
+        foreach (Utilisateur utilisateur in utilisateurs)
+        {
+            Console.Write("\nNom et Prénom : " + utilisateur.GetNomComplet() + " - ");
+
+            Console.Write(
+                "Date de naissance (âge): " +
+                utilisateur.GetDateDeNaissance() +
+                " (" + utilisateur.GetAge() + ") - "
+                );
+
+            Console.WriteLine(utilisateur.GetCouleurOuMetier());
+
+
+        }
+
+
+
+
+        /*List<Utilisateur> resultat = utilisateurs.FindAll(u => u.IsMajeur() && u.GetDateDeNaissance() == DateTime.Now.ToShortDateString());
+
+        foreach (Utilisateur u in utilisateurs)
+        {
+            if(u.IsMajeur() && u.GetDateDeNaissance() == DateTime.Now.ToShortDateString())
+            {
+                resultat.Add(u);
+            }
+        }*/
+
+    }
+}
--- Algorithmes/5 Extra/Collection.Listes/Program.cs
+++ Algorithmes/5 Extra/Collection.Listes/Program.cs
@@ -65,6 +65,77 @@ internal class Program
     }
 }
 
+/*
+   Console.WriteLine("Enregistrement d'utilisateurs (gestion avec Listes).");
+
+        //Variable
+        DateTime ajd = DateTime.Now;
+        int age = 0;
+        string saisieNomPrenom;
+        string saisieDate;
+        string? metierCouleur = null;
+        ConsoleKey continuerO_N;
+        List<string[]> utilisateurs = new();
+
+        //Traitement
+        do
+        {
+            Console.WriteLine("Saisissez le nom et Prénom.");
+            saisieNomPrenom = Console.ReadLine();
+
+            Console.WriteLine("Saisissez la date de naissane, jour/mois/année.");
+            saisieDate = Console.ReadLine();
+
+            if (DateTime.TryParse(saisieDate, out DateTime formaDate))
+            {
+                TimeSpan intervalle = ajd - formaDate;
+                age = (int)(intervalle.Days / 365.25);
+            }
+
+            if (age < 0)
+            {
+                Console.WriteLine("Saisie non valide.");
+            }
+            else if (age < 18)
+            {
+                Console.WriteLine("couleur préférée ?");
+                metierCouleur = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Votre metier ?");
+                metierCouleur = Console.ReadLine();
+            }
+
+            string[] unUtilisateur = [saisieNomPrenom, saisieDate, age.ToString(), metierCouleur];
+
+            utilisateurs.Add(unUtilisateur);
+
+            Console.WriteLine("Voulez vous saisir un autre utilisateur : N/O ?");
+            continuerO_N = Console.ReadKey(true).Key;
+        }
+        while (continuerO_N == ConsoleKey.O);
+
+        // Affichage de la liste des utilisateurs
+
+        // List<string[]> utilisateurs = new();
+        foreach (string[] utilisateur in utilisateurs)
+        {
+            Console.Write("Nom et Prénom: " + utilisateur[0] + " - ");
+            Console.Write("Date de naissance (âge) : " + utilisateur[1] + "(" + utilisateur[2] + ")");
+
+            if (int.Parse(utilisateur[2]) < 18)
+            {
+                Console.WriteLine(" Couleur: " + utilisateur[3]);
+            }
+            else
+            {
+                Console.WriteLine(" Métier: " + utilisateur[3]);
+            }
+
+        }
+
+ */
 
 
 
