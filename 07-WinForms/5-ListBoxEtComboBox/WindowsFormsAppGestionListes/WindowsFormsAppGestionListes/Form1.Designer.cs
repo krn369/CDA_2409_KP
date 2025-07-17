65559d3d2b29bf7ddf1cb0fb6b47b3309650ab35
KARAN PARIYAR <144631087+krn369@users.noreply.github.com>
2025-07-17 17:09:12 +0200

    up

---
+++ 07-WinForms/5-ListBoxEtComboBox/WindowsFormsAppGestionListes/WindowsFormsAppGestionListes/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WindowsFormsAppGestionListes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboSource = new System.Windows.Forms.ComboBox();
+            this.listCible = new System.Windows.Forms.ListBox();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnAjouterTous = new System.Windows.Forms.Button();
+            this.btnRetourner = new System.Windows.Forms.Button();
+            this.btnRetournerTous = new System.Windows.Forms.Button();
+            this.btnMonter = new System.Windows.Forms.Button();
+            this.btnDescendre = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboSource
+            // 
+            this.comboSource.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboSource.FormattingEnabled = true;
+            this.comboSource.Location = new System.Drawing.Point(12, 40);
+            this.comboSource.Name = "comboSource";
+            this.comboSource.Size = new System.Drawing.Size(163, 24);
+            this.comboSource.TabIndex = 0;
+            this.comboSource.DropDown += new System.EventHandler(this.comboSource_DropDown);
+            this.comboSource.SelectedIndexChanged += new System.EventHandler(this.comboSource_SelectedIndexChanged);
+            // 
+            // listCible
+            // 
+            this.listCible.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listCible.FormattingEnabled = true;
+            this.listCible.ItemHeight = 16;
+            this.listCible.Location = new System.Drawing.Point(326, 40);
+            this.listCible.Name = "listCible";
+            this.listCible.Size = new System.Drawing.Size(157, 180);
+            this.listCible.TabIndex = 1;
+            this.listCible.SelectedIndexChanged += new System.EventHandler(this.listCible_SelectedIndexChanged);
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAjouter.Location = new System.Drawing.Point(212, 40);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(75, 23);
+            this.btnAjouter.TabIndex = 2;
+            this.btnAjouter.Text = ">";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // btnAjouterTous
+            // 
+            this.btnAjouterTous.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAjouterTous.Location = new System.Drawing.Point(212, 69);
+            this.btnAjouterTous.Name = "btnAjouterTous";
+            this.btnAjouterTous.Size = new System.Drawing.Size(75, 23);
+            this.btnAjouterTous.TabIndex = 3;
+            this.btnAjouterTous.Text = ">>";
+            this.btnAjouterTous.UseVisualStyleBackColor = true;
+            this.btnAjouterTous.Click += new System.EventHandler(this.btnAjouterTous_Click);
+            // 
+            // btnRetourner
+            // 
+            this.btnRetourner.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRetourner.Location = new System.Drawing.Point(212, 166);
+            this.btnRetourner.Name = "btnRetourner";
+            this.btnRetourner.Size = new System.Drawing.Size(75, 23);
+            this.btnRetourner.TabIndex = 4;
+            this.btnRetourner.Text = "<";
+            this.btnRetourner.UseVisualStyleBackColor = true;
+            this.btnRetourner.Click += new System.EventHandler(this.btnRetourner_Click);
+            // 
+            // btnRetournerTous
+            // 
+            this.btnRetournerTous.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRetournerTous.Location = new System.Drawing.Point(212, 197);
+            this.btnRetournerTous.Name = "btnRetournerTous";
+            this.btnRetournerTous.Size = new System.Drawing.Size(75, 23);
+            this.btnRetournerTous.TabIndex = 5;
+            this.btnRetournerTous.Text = "<<";
+            this.btnRetournerTous.UseVisualStyleBackColor = true;
+            this.btnRetournerTous.Click += new System.EventHandler(this.btnRetournerTous_Click);
+            // 
+            // btnMonter
+            // 
+            this.btnMonter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMonter.Location = new System.Drawing.Point(381, 244);
+            this.btnMonter.Name = "btnMonter";
+            this.btnMonter.Size = new System.Drawing.Size(29, 31);
+            this.btnMonter.TabIndex = 6;
+            this.btnMonter.Text = "↑";
+            this.btnMonter.UseVisualStyleBackColor = true;
+            this.btnMonter.Click += new System.EventHandler(this.btnMonter_Click);
+            // 
+            // btnDescendre
+            // 
+            this.btnDescendre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDescendre.Location = new System.Drawing.Point(416, 244);
+            this.btnDescendre.Name = "btnDescendre";
+            this.btnDescendre.Size = new System.Drawing.Size(29, 31);
+            this.btnDescendre.TabIndex = 7;
+            this.btnDescendre.Text = "↓";
+            this.btnDescendre.UseVisualStyleBackColor = true;
+            this.btnDescendre.Click += new System.EventHandler(this.btnDescendre_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(495, 298);
+            this.Controls.Add(this.btnDescendre);
+            this.Controls.Add(this.btnMonter);
+            this.Controls.Add(this.btnRetournerTous);
+            this.Controls.Add(this.btnRetourner);
+            this.Controls.Add(this.btnAjouterTous);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.listCible);
+            this.Controls.Add(this.comboSource);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Les listes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboSource;
+        private System.Windows.Forms.ListBox listCible;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnAjouterTous;
+        private System.Windows.Forms.Button btnRetourner;
+        private System.Windows.Forms.Button btnRetournerTous;
+        private System.Windows.Forms.Button btnMonter;
+        private System.Windows.Forms.Button btnDescendre;
+    }
+}
+
---
+++ 07-WinForms/5-ListBoxEtComboBox/WindowsFormsAppGestionListes/WindowsFormsAppGestionListes/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppGestionListes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // Initialize the Form
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialize source countries
+            string[] paysSource = { "France", "Belgique", "Allemagne", "Japon", "Portugal", "Grèce" };
+
+            // Add countries to the comboBox
+            comboSource.Items.AddRange(paysSource);
+
+            // Initialize target countries
+            string[] paysCible = { "Bulgarie", "Espagne" };
+
+            // Add countries to the ListBox
+            listCible.Items.AddRange(paysCible);
+
+            // Disable buttons initially
+            UpdateButtonStates();
+        }
+
+
+        // Implement the Button Functions 
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            // Check if an item is selected in the ComboBox
+            if (comboSource.SelectedIndex != -1)
+            {
+                // Add the selected item to the ListBox
+                listCible.Items.Add(comboSource.SelectedItem);
+
+                // Remove the items from the ComboBox
+                comboSource.Items.Remove(comboSource.SelectedItem);
+
+                //Update button states
+                UpdateButtonStates();
+            }
+        }
+
+        private void btnAjouterTous_Click(object sender, EventArgs e)
+        {
+            // Add all items from ComboBox to ListBox
+            foreach (var item in comboSource.Items)
+            {
+                listCible.Items.Add(item);
+            }
+
+            // Clear the ComboBox
+            comboSource.Items.Clear();
+
+            // Update button states
+            UpdateButtonStates();
+        }
+
+        private void btnRetourner_Click(object sender, EventArgs e)
+        {
+            // Check if an item is selected in the ListBox
+            if(listCible.SelectedIndex != -1)
+            {
+                // Add the selected item to the ComboBox
+                comboSource.Items.Add(listCible.SelectedItem);
+
+                // Remove the items from the ListBox
+                listCible.Items.Remove(listCible.SelectedItem);
+
+                // Update button statezs
+                UpdateButtonStates();
+                    
+            }
+        }
+
+        private void btnRetournerTous_Click(object sender, EventArgs e)
+        {
+            // Add all items from ListBox to ComboBox
+            foreach (var item in listCible.Items)
+            {
+                comboSource.Items.Add(item);
+            }
+
+            // Clear the ListBox
+            listCible.Items.Clear();
+
+            // Update button states
+            UpdateButtonStates();
+        }
+
+        private void btnMonter_Click(object sender, EventArgs e)
+        {
+            if (listCible.SelectedIndex > 0) // Can't move first item up
+            {
+                int selectedIndex = listCible.SelectedIndex;
+                object selectedItem = listCible.SelectedItem;
+
+                // Remove the item
+                listCible.Items.Remove(selectedItem);
+
+                // Reinsert it one position higher
+                listCible.Items.Insert(selectedIndex - 1, selectedItem);
+
+                // Reselect the item
+                listCible.SelectedIndex = selectedIndex - 1;
+
+                // Update button states
+                UpdateButtonStates();
+            }
+        }
+
+        private void btnDescendre_Click(object sender, EventArgs e)
+        {
+            if (listCible.SelectedIndex < listCible.Items.Count - 1 && listCible.SelectedIndex != -1)
+            {
+                int selectedIndex = listCible.SelectedIndex;
+                object selectedItem = listCible.SelectedItem;
+
+                // Remove the item
+                listCible.Items.Remove(selectedItem);
+
+                // Reinsert it one position lower
+                listCible.Items.Insert(selectedIndex + 1, selectedItem);
+
+                // Reselect the item
+                listCible.SelectedIndex = selectedIndex + 1;
+
+                // Update button states
+                UpdateButtonStates();
+            }
+        }
+
+        private void UpdateButtonStates()
+        {
+            // Enable/disable Add button based on ComboBox selection
+            btnAjouter.Enabled = comboSource.SelectedIndex != -1;
+
+            // Enable/disable Add All button based on ComboBox content
+            btnAjouterTous.Enabled = comboSource.Items.Count > 0;
+
+            // Enable/disable Return button based on ListBox selection
+            btnRetourner.Enabled = listCible.SelectedIndex != -1;
+
+            // Enable/disable Return All button based on ListBox content
+            btnRetournerTous.Enabled = listCible.Items.Count > 0;
+
+            // Enable/disable Move Up button
+            btnMonter.Enabled = listCible.SelectedIndex > 0;
+
+            // Enable/disable Move Down button
+            btnDescendre.Enabled = listCible.SelectedIndex != -1 &&
+                                  listCible.SelectedIndex < listCible.Items.Count - 1;
+        }
+
+        private void comboSource_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateButtonStates();
+        }
+
+        private void listCible_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateButtonStates();
+        }
+
+        private void comboSource_DropDown(object sender, EventArgs e)
+        {
+            string newItem = comboSource.Text.Trim();
+
+            if (!string.IsNullOrEmpty(newItem) &&
+                !comboSource.Items.Contains(newItem) &&
+                !listCible.Items.Contains(newItem))
+            {
+                comboSource.Items.Add(newItem);
+                comboSource.Text = ""; // Clear the text after adding
+            }
+        }
+    }
+}
